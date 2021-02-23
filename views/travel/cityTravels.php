3c20a82c0651550f2922327a4494985411eb1639
cirne <cirnemiro@gmail.com>
2021-02-23 11:35:39 +0100

    css

--- views/travel/cityTravels.php
+++ views/travel/cityTravels.php
@@ -10,6 +10,7 @@ if (isset($_REQUEST['message'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>City Travels</title>
   <script defer src="./assets/js/utils.js"></script>
 </head>
--- views/travel/travelDashboard.php
+++ views/travel/travelDashboard.php
@@ -10,6 +10,7 @@ if (isset($_REQUEST['message'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Dashboard</title>
   <script defer src="./assets/js/utils.js"></script>
 </head>
@@ -21,6 +22,7 @@ if (isset($_REQUEST['message'])) {
       <th>Name</th>
       <th>Last Name</th>
       <th>City</th>
+      <th>Buttons</th>
     </tr>
     <?php
     if (isset($travels)) {
